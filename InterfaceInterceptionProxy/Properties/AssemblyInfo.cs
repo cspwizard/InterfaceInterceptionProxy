bb8143c3eb50b0bafb42304f1ecb5f9a5f0a64c9
Konstantin Pozdniakov <kpozdniakov@gmail.com>
2016-08-28 21:41:01 +0300

    NuGet packaging preparation

--- InterfaceInterceptionProxy/Properties/AssemblyInfo.cs
+++ InterfaceInterceptionProxy/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following set of attributes.
 // Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("InterfaceInterceptionProxy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Interface Interceptor Proxy Type generator that implement an interface, and proxy calls to defined with attribute methods via defined intercepting handlers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Konstantin Pozdniakov")]
 [assembly: AssemblyProduct("InterfaceInterceptionProxy")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
